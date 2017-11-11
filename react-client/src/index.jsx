6dfb9022f0fe36f395b44d99a09679f3ba6c19b7
Bernard Kitchens <bernardkitchens@Bernards-MacBook-Pro.local>
2017-11-11 11:34:49 -0800

    finally rendering fake data, remember to use a different get route

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -8,51 +8,19 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-    //dummy data is here for now...needs to move to database
-      snakes: [
-        {
-          species: 'King Cobra',
-          photoLink: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG-NxFQ5w1eW7JiobvVm3ppsR62eMtPN1fIK6XOAfEaHr9dHuxPQ`,
-          biteProtocol: `http://toxicology.ucsd.edu/Snakebite%20Protocols/Ophiopha.htm`, 
-          notes: `Have a MINIMUM of 10 vials minimum of Tiger Snake Antivenom available`
-        },
-        {
-          species: 'Black Mamba',
-          photoLink: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG-NxFQ5w1eW7JiobvVm3ppsR62eMtPN1fIK6XOAfEaHr9dHuxPQ`, 
-          biteProtocol: `http://toxicology.ucsd.edu/Snakebite%20Protocols/Dendroa3.htm`, 
-          notes: `Have a MINIMUM of 10 vials of SAIMR Polyvalent Antivenom available`
-        },
-        {
-          species: 'Fer De Lance',  
-          photoLink: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG-NxFQ5w1eW7JiobvVm3ppsR62eMtPN1fIK6XOAfEaHr9dHuxPQ`,  
-          biteProtocol: `http://toxicology.ucsd.edu/Snakebite%20Protocols/Bothrops.htm`, 
-          notes: `Have a MINIMUM of 20 vials of Wyeth Crotalidae Polyvalent Antivenom`
-        },
-        {
-          species: 'Coastal Taipan',
-          photoLink: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG-NxFQ5w1eW7JiobvVm3ppsR62eMtPN1fIK6XOAfEaHr9dHuxPQ`,  
-          biteProtocol: `http://toxicology.ucsd.edu/Snakebite%20Protocols/Oxyura~2.htm`, 
-          notes: `Have a MINIMUM of 10 vials of Taipan Antivenom available`
-        },
-        {
-          species: 'Western Green Mamba', 
-          photoLink: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG-NxFQ5w1eW7JiobvVm3ppsR62eMtPN1fIK6XOAfEaHr9dHuxPQ`, 
-          biteProtocol: `http://toxicology.ucsd.edu/Snakebite%20Protocols/Dendroa4.htm`, 
-          notes: `Have a MINIMUM of 10 vials of Taipan Antivenom available`
-        }
-      ]
+    //dummy data was here for now...needs to move to database
+      snakes: []
     }
   }
 
   componentDidMount() {
     $.ajax({
       method: 'GET',
-      url: '/', 
+      url: '/getsnakes', 
       dataType: 'json',
       success: (data) => {
-        this.setState({
-          snakes: data
-        })
+        this.setState({snakes:data});
+        console.log('you got data', data);
       },
       error: (err) => {
         console.log('err', err);
--- server/index.js
+++ server/index.js
@@ -1,26 +1,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+
+//brought fakeData into server module
+var fakeData = require('../database-mongo/fakedata');
+
+
+
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 //var items = require('../database-mysql');
 //var items = require('../database-mongo');
 
 var app = express();
 
-
 //bodyparser ensures that the body is NOT empty - USE BOTH!!!!
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.send(data);
-    }
-  });
+//can't use the same endpoint as a index file
+app.get('/getsnakes', function (req, res) {
+  res.statusCode = 200;
+  //console.log(fakeData);
+  res.send(fakeData);
 });
+
 
 //post route to add snakes to our database
 //post is working
